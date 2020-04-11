3e869cfe1cb6f59653670ab8094eee2ed2d77e13
Oriol Tobar <otobar@visual-engin.com>
2020-04-11 16:47:33 +0200

    Created ProfileFragment, modified flow in nav_graph. Added strings.

--- app/src/main/java/com/napptilians/doy/view/user/ProfileFragment.kt
+++ app/src/main/java/com/napptilians/doy/view/user/ProfileFragment.kt
@@ -4,15 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.napptilians.commons.error.ErrorModel
+import com.napptilians.domain.models.user.UserModel
 import com.napptilians.doy.R
 import com.napptilians.doy.base.BaseFragment
+import com.napptilians.doy.extensions.gone
+import com.napptilians.doy.extensions.visible
+import com.napptilians.features.UiStatus
+import com.napptilians.features.viewmodel.ProfileViewModel
 import kotlinx.android.synthetic.main.profile_fragment.profileEditMode
+import kotlinx.android.synthetic.main.profile_fragment.profileFragmentProgressView
 import kotlinx.android.synthetic.main.profile_fragment.profileInfoFrameLayout
+import kotlinx.android.synthetic.main.profile_fragment.profileInfoLogOutText
 
 class ProfileFragment : BaseFragment() {
 
     private var isEditMode: Boolean = false
+
+    private val viewModel: ProfileViewModel by viewModels { vmFactory }
+
+    private val editModeView: ProfileEditView? by lazy {
+        context?.let { ProfileEditView(it) }
+    }
+
+    private val readModeView: ProfileReadView? by lazy {
+        context?.let { ProfileReadView(it) }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,21 +48,68 @@ class ProfileFragment : BaseFragment() {
         profileEditMode.setOnClickListener {
             switchEditMode()
         }
+
+        profileInfoLogOutText.setOnClickListener {
+            viewModel.logout()
+        }
+
+        viewModel.userDataStream.observe(
+            viewLifecycleOwner,
+            Observer<UiStatus<UserModel, ErrorModel>> {
+                handleUiStates(it, ::processNewValue)
+            }
+        )
+
+        viewModel.updateUserDataStream.observe(
+            viewLifecycleOwner,
+            Observer<UiStatus<UserModel, ErrorModel>> {
+                handleUiStates(it, ::processNewValue)
+            }
+        )
+
+        viewModel.logoutDataStream.observe(
+            viewLifecycleOwner,
+            Observer<UiStatus<Unit, ErrorModel>> {
+                handleUiStates(it) { logout() }
+            }
+        )
+    }
+
+    private fun processNewValue(user: UserModel) {
+        profileFragmentProgressView.gone()
+        if (isEditMode) {
+            return
+        }
+        readModeView?.apply {
+            updateFields(user)
+            profileInfoFrameLayout.removeAllViews()
+            profileInfoFrameLayout.addView(this)
+        }
     }
 
     override fun onError(error: ErrorModel) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        profileFragmentProgressView.gone()
+        val errorString = error.message
+            ?.takeIf { it.isNotEmpty() }
+            ?.let { it }
+            ?: run { getString(R.string.generic_error) }
+
+        Toast.makeText(activity, errorString, Toast.LENGTH_LONG).show()
     }
 
     override fun onLoading() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        profileFragmentProgressView.visible()
+    }
+
+    private fun logout() {
+        val direction = ProfileFragmentDirections.actionProfileFragmentToIntroFragment()
+        findNavController().navigate(direction)
     }
 
     private fun defaultView() {
         context?.let {
-            val readView = ProfileReadView(it)
             profileInfoFrameLayout.removeAllViews()
-            profileInfoFrameLayout.addView(readView)
+            profileInfoFrameLayout.addView(readModeView)
         }
     }
 
