8faabe26020310b57dc97e4aa4bd5225814e8ec9
jlmari <jlmari@visual-engin.com>
2020-04-11 13:22:18 +0200

    disable keyboard when clicking outside edit text

--- app/src/main/java/com/napptilians/doy/MainActivity.kt
+++ app/src/main/java/com/napptilians/doy/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.napptilians.doy
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.napptilians.doy.base.BaseActivity
@@ -34,5 +40,28 @@ class MainActivity : BaseActivity() {
         navBottom.setOnNavigationItemSelectedListener {
             true
         }
+    }
+
+    /**
+     * This method overrides the default dispatch touch event from the Android [AppCompatActivity].
+     * It clears the focus of [EditText] when a touch event is triggered outside.
+     *
+     * @param event The [MotionEvent] executed by the user when touching the screen
+     * @return The [Boolean] with the default behaviour of [dispatchTouchEvent]
+     */
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        // Clear Edit Text focus if any
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            (currentFocus as? EditText)?.run {
+                val outRect = Rect().apply { getGlobalVisibleRect(this) }
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    clearFocus()
+                    (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)
+                        ?.hideSoftInputFromWindow(windowToken, 0)
+                }
+            }
+        }
+        // Return default behaviour
+        return super.dispatchTouchEvent(event)
     }
 }
