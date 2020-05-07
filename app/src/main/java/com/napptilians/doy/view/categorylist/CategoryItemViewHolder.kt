b1f54841feef7f0233aac0ceab4e6f38eabee4cc
jlmari <jlmari@visual-engin.com>
2020-05-07 11:43:52 +0200

    add opacity to category items to see the text without troubles

--- app/src/main/java/com/napptilians/doy/view/categorylist/CategoryItemViewHolder.kt
+++ app/src/main/java/com/napptilians/doy/view/categorylist/CategoryItemViewHolder.kt
@@ -1,13 +1,19 @@
 package com.napptilians.doy.view.categorylist
 
+import android.graphics.drawable.Drawable
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.napptilians.domain.models.category.CategoryModel
 import com.napptilians.doy.R
 import com.napptilians.doy.base.BaseViewHolder
 import com.napptilians.doy.extensions.gone
 import com.napptilians.doy.extensions.visible
 import kotlinx.android.synthetic.main.category_item.view.categoryImage
+import kotlinx.android.synthetic.main.category_item.view.categoryOpacity
 import kotlinx.android.synthetic.main.category_item.view.categoryText
 import kotlinx.android.synthetic.main.category_item.view.tickImageView
 
@@ -32,6 +38,27 @@ class CategoryItemViewHolder constructor(parent: ViewGroup, private val isAdding
         Glide.with(itemView)
             .load(model.pictureUrl)
             .placeholder(R.color.colorAccent)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirst: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirst: Boolean
+                ): Boolean {
+                    itemView.categoryOpacity.visible()
+                    return false
+                }
+            })
             .into(itemView.categoryImage)
     }
 }
