4706be0ff30f15579042ec046604bd9bbe06d565
Francesc Pérez <francesc_pp@hotmail.es>
2020-05-18 19:34:45 -0700

    Update IntroFragment.kt

--- app/src/main/java/com/napptilians/doy/view/intro/IntroFragment.kt
+++ app/src/main/java/com/napptilians/doy/view/intro/IntroFragment.kt
@@ -11,6 +11,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.napptilians.commons.error.ErrorModel
 import com.napptilians.doy.R
